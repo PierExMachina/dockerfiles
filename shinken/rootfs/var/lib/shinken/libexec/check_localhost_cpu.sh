099bba03c19f5ab39351246e7159f4a0654af819
xataz <xataz@mondedie.fr>
2017-06-26 22:31:18 +0200

    Fix(shinken) : Fix memory && cpu script

--- shinken/rootfs/var/lib/shinken/libexec/check_localhost_cpu.sh
+++ shinken/rootfs/var/lib/shinken/libexec/check_localhost_cpu.sh
@@ -5,11 +5,11 @@ CPU_CRIT=$2
 
 RESULT=$(top -n 1 | grep -E ^CPU: | awk '{print 100 - substr($8, 1, length($8)-1)}')
 
-if [ $RESULT -gt CPU_WARN ] && [ $RESULT -le CPU_CRIT ] ; then
+if [ $RESULT -gt $CPU_WARN ] && [ $RESULT -le $CPU_CRIT ] ; then
     echo "Usage : ${RESULT}% | status=${RESULT}%;${CPU_WARN};${CPU_CRIT};0;100"
     top -n 1 | grep -E ^CPU:
     exit 1
-elif [ $RESULT -gt CPU_CRIT ]; then
+elif [ $RESULT -gt $CPU_CRIT ]; then
     echo "Usage : ${RESULT}% | status=${RESULT}%;${CPU_WARN};${CPU_CRIT};0;100"
     top -n 1 | grep -E ^CPU:
     exit 2
--- shinken/rootfs/var/lib/shinken/libexec/check_localhost_mem.sh
+++ shinken/rootfs/var/lib/shinken/libexec/check_localhost_mem.sh
@@ -5,11 +5,11 @@ MEM_CRIT=$2
 
 MEM_FREE=$(free | grep Mem: | awk '{print (($2 - $4 - $5 -$6 - $7)*100/$2)}' | cut -d. -f1)
 
-if [ $MEM_FREE -gt MEM_WARN ] && [ $MEM_FREE -le MEM_CRIT ]; then
+if [ $MEM_FREE -gt $MEM_WARN ] && [ $MEM_FREE -le $MEM_CRIT ]; then
     echo "Usage : ${MEM_FREE}% | status=${MEM_FREE}%;${MEM_WARN};${MEM_CRIT};0;100"
     free
     exit 1
-elif [ $MEM_FREE -gt MEM_CRIT ]; then
+elif [ $MEM_FREE -gt $MEM_CRIT ]; then
     echo "Usage : ${MEM_FREE}% | status=${MEM_FREE}%;${MEM_WARN};${MEM_CRIT};0;100"
     free
     exit 2
